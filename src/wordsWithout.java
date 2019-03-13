520a0aa03e23151284e23ef4a27fa10564fc3e51
JCoopz <cooper.justin13@svvsd.org>
2019-03-13 13:57:19 -0600

    Commiting a coding bat problem but it needs work some syntax is off

---
+++ src/wordsWithout.java
@@ -0,0 +1,23 @@
+
+/**
+ * Created by Mark on 3/13/2019.
+ */
+
+public class wordsWithout {
+
+
+}
+    public static int[] copyEvens(int[] nums, int count) {
+        int[] evens;
+        for (int i = 0; i <= nums.length; i++){
+            if (nums[i] % 2 == 0){
+                evens.add[i];
+            }
+            else {
+
+            }
+
+        }
+
+        return evens;
+    }
