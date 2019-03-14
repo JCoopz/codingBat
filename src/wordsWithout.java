b1a10fed595c460f326e2d14ebf82c5bc1782914
JCoopz <cooper.justin13@svvsd.org>
2019-03-14 13:07:09 -0600

    Updated changes is an attempt to get this to function

--- src/wordsWithout.java
+++ src/wordsWithout.java
@@ -2,22 +2,35 @@
 /**
  * Created by Mark on 3/13/2019.
  */
-
+import java.util.*;
 public class wordsWithout {
+    public static void main (String [] args) {
+        int[] nums = new int[6];
+        nums[0] = 3;
+        nums[1] = 5;
+        nums[2] = 6;
+        nums[3] = 7;
+        nums[4] = 2;
 
+        System.out.println(nums, 1);
+
+    }
 
 }
-    public static int[] copyEvens(int[] nums, int count) {
-        int[] evens;
-        for (int i = 0; i <= nums.length; i++){
-            if (nums[i] % 2 == 0){
-                evens.add[i];
-            }
-            else {
+    public static int copyEvens(int[] nums, int count) {
+        ArrayList<Integer> evens= new ArrayList<Integer>();
+        int evenCount =0;
+        while (evenCount <= count) {
+            for (int i = 0; i <= nums.length; i++) {
+                if (nums[i] % 2 == 0) {
+                    evens.add(i);
+                    count++;
+                }
+
 
             }
-
         }
 
         return evens;
     }
+}
