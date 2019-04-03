a1983a1ed7333d9f1fa5311bed3e8fde35f690fd
JCoopz <cooper.justin13@svvsd.org>
2019-04-02 19:32:41 -0600

    Finished

--- src/wordsWithout.java
+++ src/wordsWithout.java
@@ -7,28 +7,30 @@ public class wordsWithout {
     public static void main (String [] args) {
         int[] nums = new int[6];
         nums[0] = 3;
-        nums[1] = 5;
+        nums[1] = 4;
         nums[2] = 6;
         nums[3] = 7;
         nums[4] = 2;
 
-        System.out.println(nums, 1);
+        System.out.println(copyEvens(nums, 3));
 
     }
 
-}
-    public static int copyEvens(int[] nums, int count) {
-        ArrayList<Integer> evens= new ArrayList<Integer>();
-        int evenCount =0;
-        while (evenCount <= count) {
-            for (int i = 0; i <= nums.length; i++) {
+    public static ArrayList<Integer> copyEvens(int[] nums, int count) {
+        ArrayList<Integer> evens = new ArrayList<Integer>();
+        int evenCount = 0;
+
+        for (int i = 0; i < nums.length; i++) {
                 if (nums[i] % 2 == 0) {
-                    evens.add(i);
-                    count++;
+                    evens.add(nums[i]);
+                    evenCount++;
                 }
-
-
-            }
+                if(evenCount == count){
+                  //System.out.println("Entered");
+                  //return evens;
+                    break;
+                }
+                //System.out.println(evenCount);
         }
 
         return evens;
